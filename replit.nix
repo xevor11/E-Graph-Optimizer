2beb8145632ce2378f658cfc80602d941071c3a0
Vedant Tewari <74680679+Vtewari2311@users.noreply.github.com>
2023-03-25 23:30:44 +0000

    File Directory

---
+++ replit.nix
@@ -0,0 +1,9 @@
+{ pkgs }: {
+    deps = [
+        (pkgs.haskellPackages.ghcWithPackages (pkgs: [
+            # Put your dependencies here!
+        ]))
+        pkgs.haskell-language-server
+        pkgs.cabal-install
+    ];
+}
