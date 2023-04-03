2288f95a363a71bc97561b9b1c6cd26d3dc80749
Vedant Tewari <74680679+Vtewari2311@users.noreply.github.com>
2023-04-03 09:35:00 -0500

    Update Main.hs

--- app/Main.hs
+++ app/Main.hs
@@ -1,4 +1,33 @@
 module Main where
 
+import Compiler.Lexer
+import Compiler.Parser
+import Compiler.SemanticAnalyzer
+import Compiler.SymbolTable
+import Compiler.CodeGenerator
+
 main :: IO ()
-main = putStrLn "Hello, Haskell!"
+main = do
+  -- Parse command line arguments
+  -- ...
+
+  -- Read input file
+  input <- readFile "input_file.txt"
+
+  -- Lex input
+  let tokens = tokenize input
+
+  -- Parse tokens
+  let ast = parse tokens
+
+  -- Perform semantic analysis
+  let analyzedAst = analyze ast
+
+  -- Generate symbol table
+  let symbolTable = generateSymbolTable analyzedAst
+
+  -- Generate code
+  let code = generateCode analyzedAst symbolTable
+
+  -- Write output file
+  writeFile "output_file.txt" code
